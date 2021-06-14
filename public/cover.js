77f36592c8aa2d680d9785d18b276777b9dc7be6
Dogun123 <tpdnd7890@gmail.com>
2021-06-14 23:27:08 +0900

    home background make

--- public/cover.js
+++ public/cover.js
@@ -59,3 +59,6 @@ document.addEventListener('scroll',()=>{
 
 
 
+
+
+
--- router/main.js
+++ router/main.js
@@ -3,7 +3,7 @@ module.exports = function(app)
     app.get('/',function(req,res){
         res.render('cover.html')
     });
-    app.get('/about',function(req,res){
-        res.render('about.html');
+    app.get('/home',function(req,res){
+        res.render('home.html');
     });
 }
