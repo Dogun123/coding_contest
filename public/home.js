739f0610aa4e5e5de14091c53224e75c14fc5f51
Dogun123 <tpdnd7890@gmail.com>
2021-06-17 23:47:59 +0900

    nav_sub2

--- public/home.js
+++ public/home.js
@@ -1,9 +1,38 @@
 'use strict';
 
-const navbarToggleBtn = document.querySelector(".nav__links.nav__links--left");
-const navbarSub = document.querySelector("#navbar__sub");
+const linkItems = document.querySelectorAll(".nav__text");
+const dropdownMenus = document.querySelectorAll(".dropdown_menu");
 
-navbarToggleBtn.addEventListener('click',()=>{
-    navbarSub.classList.toggle('open');
-    console.log('hi')
+
+linkItems[0].addEventListener('click',()=>{
+    dropdownMenus[0].classList.toggle('open');
+    if(dropdownMenus[1].className === "dropdown_menu open"){
+        dropdownMenus[1].classList.remove('open');
+    } else if(dropdownMenus[2].className === "dropdown_menu open"){
+        dropdownMenus[2].classList.remove('open');
+    }
 });
+
+linkItems[1].addEventListener('click',()=>{
+    dropdownMenus[1].classList.toggle('open');
+    if(dropdownMenus[0].className === "dropdown_menu open"){
+        dropdownMenus[0].classList.remove('open');
+    } else if(dropdownMenus[2].className === "dropdown_menu open"){
+        dropdownMenus[2].classList.remove('open');
+    }
+});
+
+linkItems[2].addEventListener('click',()=>{
+    dropdownMenus[2].classList.toggle('open');
+    if(dropdownMenus[1].className === "dropdown_menu open"){
+        dropdownMenus[1].classList.remove('open');
+    } else if(dropdownMenus[0].className === "dropdown_menu open"){
+        dropdownMenus[0].classList.remove('open');
+    }
+});
+
+
+
+
+
+
